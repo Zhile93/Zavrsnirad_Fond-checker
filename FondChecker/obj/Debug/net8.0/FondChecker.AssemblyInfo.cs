4f6ae98d2414f762a7f75a65bc8717ff2f48bb33
Zhile93 <goran.zivkovic1993@gmail.com>
2024-11-11 16:40:27 +0100

    Fin

--- FondChecker/obj/Debug/net8.0/FondChecker.AssemblyInfo.cs
+++ FondChecker/obj/Debug/net8.0/FondChecker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FondChecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+957cfd332eeab1d773ce506c1e7b48f55ab9a204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3585050049aeb3074c6bf486ed2b09b33ec240dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FondChecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FondChecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
